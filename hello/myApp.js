526922fe3a3039ade5b09217e44a8f7ba30f8a61
Meagan CK <66429738+meaganck@users.noreply.github.com>
2021-01-01 15:46:56 -0500

    Updated myApp.js and index.html
    Added dynamic grid

--- hello/myApp.js
+++ hello/myApp.js
@@ -15,17 +15,16 @@ http.createServer(function (req, res) {
       var oldpath = files.filetoupload.path;
       // change this to path file is in
 
-        //check if its valid
+        //check if the file is valid (JPG or PNG)
         if ((path.extname(files.filetoupload.name).toUpperCase() == '.PNG') ||(path.extname(files.filetoupload.name).toUpperCase() == '.JPG')){
             
-            var newpath = 'C:/Users/Chris/hello/' + files.filetoupload.name;
+            var newpath = 'C:/Users/Chris/hello/' + files.filetoupload.name; //CHANGE TO YOUR DIRECTORY ********************************************************************************************
             fs.rename(oldpath, newpath, function (err) {
                 if (err) throw err;
             });
-            path.join(newpath, 'pubic');
 
             res.writeHead(200, {'Content-Type': 'text/html'});
-            stylePage(res);
+            stylePage(res); // add CSS 
             res.write('<html><body>');
 
             makeForm(res);
@@ -61,17 +60,32 @@ var makeForm = function(res){
 }
 
 var showImages = function(res) {
-  var files = fs.readdirSync("C:/Users/Chris/hello/") ;
+  // shows all the images in the directory
+
+  let row = 1;
+  res.write('<div class="container-fluid">');
+  res.write('<div class="row">');
+
+  var files = fs.readdirSync("C:/Users/Chris/hello/") ; //CHANGE TO YOUR DIRECTORY ********************************************************************************************
     files.forEach( function (file) {
        
-      if ((path.extname(file).toUpperCase() == '.PNG') ||(path.extname(file).toUpperCase() == '.JPG')){
-        //console.log( file );      
+      if ((path.extname(file).toUpperCase() == '.PNG') ||(path.extname(file).toUpperCase() == '.JPG')){   
+        if(row >3){
+          res.write('</div><div class="row">'); // end previous row tag and add new row
+          row = 0; // reset
+        }
+        res.write('<div class="col-md-4">')
         var data = fs.readFileSync(file);
+        // add image tag
         res.write('<img src="data:image/jpeg;base64,');
         res.write(Buffer.from(data).toString('base64'));
-        res.write('" class="center-block" style="width:25%"/>');
+        res.write('" class="center-block" style="width:65%"/>');
+        res.write('</div>'); // end col tag
+        row += 1; 
       }
     });
+
+    res.write('</div>') // end tag for container-fluid
 }
 
 var stylePage = function(res){
